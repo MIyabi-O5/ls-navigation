ef59de8f3bdc07e9b328ba8bee29cb94a871676a
murakmai <halpygg@gmail.com>
2023-05-17 00:48:43 +0900

    power!!!

--- app/src/main/java/com/example/lsnavigation/MainActivity.java
+++ app/src/main/java/com/example/lsnavigation/MainActivity.java
@@ -5,9 +5,16 @@ import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.preference.PreferenceManager;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+
+import com.felhr.usbserial.UsbSerialDevice;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
@@ -30,6 +37,40 @@ public class MainActivity extends AppCompatActivity {
     MapView map;
     IMapController mapController;
 
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+
+    /**
+     * USBシリアル通信の初期化を行う
+     * @return 成功した場合はUsbSerialDevice、失敗した場合はnull
+     */
+    UsbSerialDevice initializeUsbSerial() {
+        UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+        // USBデバイス一覧を取得して、一覧からUSBシリアルデバイスを探す
+        UsbDevice usbDevice = null;
+        for (UsbDevice d : usbManager.getDeviceList().values()) {
+            // 関係ないデバイスに誤爆することがあるので、Raspberry Pi Picoが見つかるようにVendorIdもチェックする
+            if (UsbSerialDevice.isSupported(d) && UsbSerialDevice.isCdcDevice(d) && d.getVendorId() == 0x2E8A) {
+                usbDevice = d;
+            }
+        }
+        if (usbDevice == null) {return null;}
+
+        // 権限チェック
+        if (!usbManager.hasPermission(usbDevice)) {
+            // ユーザからアクセス許可をもらっていなかったら、許可を求めたうえで初期化は失敗扱いにしてnullを返す。
+            // 一度ユーザが許可をすれば、次回以降のアプリ起動時に接続ができるようになる。
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+            usbManager.requestPermission(usbDevice, pendingIntent);
+            return null;
+        }
+
+        // 権限があるのでデバイスに接続する
+        UsbDeviceConnection usbDeviceConnection = usbManager.openDevice(usbDevice);
+        return UsbSerialDevice.createUsbSerialDevice(usbDevice, usbDeviceConnection);
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +82,8 @@ public class MainActivity extends AppCompatActivity {
 
         // 初期化
         findViews();
+
+        UsbSerialDevice serial = initializeUsbSerial();
 
         /*
         map.setUseDataConnection(false);
@@ -61,8 +104,8 @@ public class MainActivity extends AppCompatActivity {
         map.setMultiTouchControls(true);
         mapController.setZoom(16.0);
         Marker marker = new Marker(map);
-        GeoPoint kohnan = new GeoPoint(35.682267446330904, 139.54380069251135);
-        marker.setPosition(kohnan);
+        GeoPoint white = new GeoPoint(35.682267446330904, 139.54380069251135);
+        marker.setPosition(white);
         map.getOverlays().add(marker);
         marker.setTitle("働けカス");
         Drawable icon = getDrawable(R.drawable.white);
