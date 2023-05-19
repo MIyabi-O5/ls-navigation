25b745523cd173797c3e90f4d9479b71fd2ae197
murakami <halpygg@gmail.com>
2023-05-19 19:44:31 +0900

    sawaharaPower2

--- app/src/main/java/com/example/lsnavigation/SerialService.java
+++ app/src/main/java/com/example/lsnavigation/SerialService.java
@@ -9,9 +9,13 @@ import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.felhr.usbserial.UsbSerialDevice;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -24,6 +28,8 @@ public class SerialService extends Service {
 
     public static final String ACTION = "SerialService Action";
     private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+
+    UsbSerialDevice serial;
 
     Context context;
 
@@ -48,7 +54,8 @@ public class SerialService extends Service {
         if (!usbManager.hasPermission(usbDevice)) {
             // ユーザからアクセス許可をもらっていなかったら、許可を求めたうえで初期化は失敗扱いにしてnullを返す。
             // 一度ユーザが許可をすれば、次回以降のアプリ起動時に接続ができるようになる。
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+            // Flag->PendingIntent.FLAG_MUTABLE or FLAG.IMMUTABLE を設定しなければいけない
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_MUTABLE);
             usbManager.requestPermission(usbDevice, pendingIntent);
             return null;
         }
@@ -67,6 +74,12 @@ public class SerialService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.i(TAG, "onCreate");
+        serial = initializeUsbSerial();
+        if (serial == null) {
+            // USBシリアルの初期化に失敗
+            Toast.makeText(this, "connection failed", Toast.LENGTH_SHORT).show();
+            Log.i(TAG, "connection failed");
+        }
     }
 
 
@@ -78,9 +91,26 @@ public class SerialService extends Service {
         schedule = Executors.newSingleThreadScheduledExecutor();
         // 1000msecごとに処理を実行
         schedule.scheduleAtFixedRate(()->{
-            // 現在時刻をブロードキャスト配信
+
+            serial.syncOpen();
+            BufferedReader serialReader = new BufferedReader(new InputStreamReader(serial.getInputStream()));
+            String buf;
+
+            // とりあえず受信する
+            try {
+                buf = serialReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+
+            // 受信していないなら何もしない
+            if (buf == null) {
+                return;
+            }
+
             Intent intent2 = new Intent(ACTION);
-            intent2.putExtra("message", (new Date()).toString());
+            intent2.putExtra("message", buf);
             // ブロードキャストの送信、受信側はレシーバーを作成しなければいけない-> BroadcastReceiver
             sendBroadcast(intent2);
             Log.i(TAG, "onStartCommand");
